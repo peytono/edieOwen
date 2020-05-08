e51915144eefc2ab3dfb4e993916649fe2bd29ee
Peyton <pcopcoeco@gmail.com>
2020-05-08 12:34:42 -0500

    about me alignment

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@ $(function() {
     $("#ediePark").click(function(){
         $('#modalOne').modal('show');
     });
+    $("#placeHolder2").click(function(){
+        $('#modalTwo').modal('show');
+    });
+    $("#placeHolder3").click(function(){
+        $('#modalThree').modal('show');
+    });
 });
